a0574351282e36676d5cda0c0b1f13b37e147131
leesochay <sochaylee@gmail.com>
2023-07-09 12:35:36 -0400

    code to get one product and one tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,8 +21,14 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findByPk(req.params.id).then((productData) => {
+    res.json(productData);
+  });
   // be sure to include its associated Category and Tag data
 });
+
+
+
 
 // create new product
 router.post('/', (req, res) => {
